50e05b143001d16979e29b83b4a61a7ee7f4df9f
GeoWaqas <141241933+GeoWaqas@users.noreply.github.com>
2023-08-03 12:53:45 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -11,7 +11,7 @@ for i in os.listdir():
 
 st.write("My Data Visualization Website")
 
-selection = st.selectbox("Select files" , filename_list)
+selection = st.multiselect("Select files" , filename_list)
 
 df = pd.read_csv(selection)
 st.dataframe(df)
@@ -21,8 +21,11 @@ x_axis = st.selectbox("select X element", el_list)
 y_axis = st.selectbox("select Y element", el_list)
 
 from bokeh.plotting import figure
-
 p= figure(x_axis_label = x_axis, y_axis_label = y_axis)
-p.triangle((df[x_axis]/10000), (df[y_axis]/10000))
+for i in selection:
+  
+  df = pd.read_csv(selection)
+  p.triangle((df[x_axis]/10000), (df[y_axis]/10000))
+
 
 st.bokeh_chart(p, use_container_width=True)
