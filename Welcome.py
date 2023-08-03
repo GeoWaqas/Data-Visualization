c45ace4c19eae2c1f39888d0ae7d218d3866f0dc
GeoWaqas <141241933+GeoWaqas@users.noreply.github.com>
2023-08-03 09:42:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,10 +1,22 @@
 import streamlit as st
 import pandas as pd
+import os
 
-st.write("hello world")
+filename_list = []
+for i in os.listdir():
+  if i.endswith("csv")
+  filename_list.append[i]
+
+st.write(filename_list)
+
+st.write("My First Website")
+
+st.selectbox("select file", filename_list)
 
 df = pd.read_csv("Emeishan.csv")
 st.dataframe(df)
 
 el_list = df.columns.tolist() [27:80]
-st.selectbox("select elements", el_list)
+x_axis = st.selectbox("select elements", el_list)
+y_axis = st.selectbox("select elements", el_list)
+
