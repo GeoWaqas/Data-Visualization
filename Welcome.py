f948e78b0dc97241c3ac45b3a14fa507240191aa
GeoWaqas <141241933+GeoWaqas@users.noreply.github.com>
2023-08-03 10:34:29 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -27,4 +27,7 @@ df = pd.read_csv("Bastar Craton.csv")
 
 p= figure(x_axis_label = "x", y_axis_label = "y")
 p.triangle(df["Mg"]/10000, df["Si"]/10000)
+p.circle(df["Al"]/10000, df["Si"]/10000)
+p.square(df["K"]/10000, df["Si"]/10000)
+
 st.bokeh_chart(p, use_container_width=True)
