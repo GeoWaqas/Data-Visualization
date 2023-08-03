117207ef84e3091b3b5a0f89c10c7cc42974a563
GeoWaqas <141241933+GeoWaqas@users.noreply.github.com>
2023-08-03 10:25:27 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import os
+from bokeh.plotting import figure, show
+from bokeh.io import output_notebook
 
 filename_list = []
 for i in os.listdir():
@@ -20,4 +22,13 @@ el_list = df.columns.tolist() [27:80]
 x_axis = st.selectbox("select x elements", el_list)
 y_axis = st.selectbox("select y elements", el_list)
 
-plt.scatter(df[x_axis], df[y_axis])
+df = pd.read_csv("Bastar Craton.csv")
+
+output_notebook()
+
+p= figure(x_axis_label = "x", y_axis_label = "y")
+p.triangle(df["Mg"]/10000, df["Si"]/10000)
+p.circle(df["Al"]/10000, df["Si"]/10000)
+p.square(df["K"]/10000, df["Si"]/10000)
+show(p)
+
